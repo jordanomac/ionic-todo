1c34123dab1a181f9ffb0b2f2dfc80fe2a67a29b
jordanpurinton <jordanomac@gmail.com>
2016-08-01 15:39:40 -0600

    added files that weren't tracked

---
+++ app/pages/item-detail/item-detail.js
@@ -0,0 +1,26 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var ionic_angular_1 = require('ionic-angular');
+var ItemDetailPage = (function () {
+    function ItemDetailPage(navParams) {
+        this.navParams = navParams;
+        this.title = this.navParams.get('item').title;
+        this.description = this.navParams.get('item').description;
+    }
+    ItemDetailPage = __decorate([
+        core_1.Component({
+            templateUrl: 'build/pages/item-detail/item-detail.html',
+        }), 
+        __metadata('design:paramtypes', [ionic_angular_1.NavParams])
+    ], ItemDetailPage);
+    return ItemDetailPage;
+})();
+exports.ItemDetailPage = ItemDetailPage;
